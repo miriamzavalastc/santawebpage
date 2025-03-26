927b21e084565793ca158c0f76f1a8a6d5a0e6d4
miriamzavalastc <miriamzavalastc@gmail.com>
2025-03-26 16:26:22 -0600

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+Use Carbon\Carbon;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Carbon::setLocale(config('app.locale'));
+        setlocale(LC_ALL, 'es_ES');
+    }
+}
